cba80cef2c30ccd4dbc6e8704177c51227d5825e
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-06-19 22:19:36 +0300

    add GitIgnore

---
+++ Home_work_2/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа. */
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+string input = Console.ReadLine() ?? "";
+
+int Number = int.Parse(input);
+int SecondNumber = Number / 10 % 10;
+
+
+Console.WriteLine($"{Number} -> {SecondNumber}");
