94c314c5eb5c8792e3c6752ad127e15978de2ec5
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-06-22 21:16:23 +0300

    add Task №23

---
+++ Home_work_3/Task_23/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая на вход принимает число (N)
+и выдает таблицу кубов чисел от 1 до N */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine() ?? "");
+
+int index = 1;
+int StringCubes = 0;
+
+while (index <= Number)
+{   
+    StringCubes = (int)Math.Pow(index, 3);
+    Console.Write($"{StringCubes} ");
+    index++;
+}
