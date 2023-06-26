dec5b6142a6d473e04f82545f8d0f8ec713ee9fa
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-06-26 22:21:24 +0300

    add Task № 29

---
+++ Home_work_4/Task_29/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран. */
+
+Console.Clear();
+
+int size = 8;
+int minValue = GetNumberFromUser("Введите минимальное значение: ", "Ошибка ввода!");
+int maxValue = GetNumberFromUser("Введите минимальное значение: ", "Ошибка ввода!");
+
+int [] array = GetArray(size, minValue, maxValue);
+
+Console.Write($"{String.Join(" ", array)} -> ");
+
+Write(array);
+
+int GetNumberFromUser(string Message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(Message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res [i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void Write (int [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        if (i + 1 == size)
+            Console.Write($"{array[i]}");
+        else 
+            Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
