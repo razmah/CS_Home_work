da14fca275833b1e16a26d16f860496d1ac2e360
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-06-26 20:35:54 +0300

    add Task № 25

---
+++ Home_work_4/Task_25/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишите цикл, который принимает на вход два числа (А и В)
+и возводит число А в натуральную степень В. 
+    3, 5 -> 243
+    2, 4 -> 16                                               */
+
+Console.Clear();
+
+int A = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int B = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+
+int result = DegreeOfNumber(A, B);
+
+Console.WriteLine($"{A}, {B} -> {result}");
+
+int GetNumberFromUser(string Message, string ErrorMessage)
+{
+    while (true)
+    {   
+        Console.Write(Message);
+        if (int.TryParse(Console.ReadLine(), out int UserNumber))
+            return UserNumber;
+        Console.WriteLine(ErrorMessage);
+    }
+
+}
+
+int DegreeOfNumber(int number_1, int number_2)
+{
+    int res = (int)Math.Pow(number_1, number_2);
+    return res;
+}
