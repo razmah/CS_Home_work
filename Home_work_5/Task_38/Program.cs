8a383d30cc6b7e27b2a83f6f23e7c7e182cac190
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-06-30 01:36:37 +0300

    add Task №38

---
+++ Home_work_5/Task_38/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу
+между максимальным и минимальным элементов массива.   */
+
+Console.Clear();
+
+int size = GetNumberFromUser("Введите размер массива: ", "Ошибка ввода!");
+double minValue = GetNumberFromUser("Введите минимальное значение массива: ", "Ошибка ввода!");
+double maxValue = GetNumberFromUser("Введите максимальное значение массива: ", "Ошибка ввода");
+
+double [] massive = GetArray(size, minValue, maxValue);
+Console.Write(String.Join($" ", massive.Select(n => $"{n:f2}")));
+
+double diff = Difference(massive);
+Console.Write($" -> {diff:f2}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int UserNumber))
+            return UserNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+double [] GetArray(int size, double minValue, double maxValue)
+{
+    double [] res = new double [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res [i] = new Random().NextDouble() * (maxValue + Math.Abs(minValue)) + minValue;
+    }
+    return res;
+}
+
+double Difference(double [] arr)
+{
+    double minNumber = arr [0];
+    double maxNumber = arr [0];
+    double sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr [i] < minNumber)
+            minNumber = arr [i];
+        if (arr [i] > maxNumber)
+            maxNumber = arr [i];
+        sum = maxNumber - minNumber;
+    }   
+    return sum;
+}
